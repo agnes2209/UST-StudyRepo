634bb302aa2c737fc9062c745b2d7c6194202677
agnes2209 <agnedavi2209@gmail.com>
2025-12-12 19:37:52 +0530

    Updated app.js

--- pages/app.js
+++ pages/app.js
@@ -2,3 +2,4 @@ console.log("Hello, World!");
 console.log("This is a simple Node.js application.");
 console.log("Enjoy coding!");
 console.log("Goodbye, World!");
+console.log("End of the application.");
